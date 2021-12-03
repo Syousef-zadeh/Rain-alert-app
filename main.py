0132a960fe895ee699a57588d3d136dc0940316a
Sheida <syousef-zadeh@myseneca.ca>
2021-12-03 10:15:50 -0500

    slice the data

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ parameters = {
     "lat": 43.653225,
     "lon": -79.383186,
     "appid": api_key,
+    "exclude": "minutely,daily,current"
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
-data = response.json()
+weather_data = response.json()
+data = weather_data["hourly"][:12]
 print(data)
+# print(weather_data["hourly"][0]["weather"][0]["id"])
+
+
