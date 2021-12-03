1ddc191af8ab50587331ab06fd01cdd8a2fc0d5d
Sheida <syousef-zadeh@myseneca.ca>
2021-12-03 10:46:39 -0500

    rain alert

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ parameters = {
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
 weather_data = response.json()
-data = weather_data["hourly"][:12]
-print(data)
+data_slice = weather_data["hourly"][:12]
+
+will_rain = False
+for weather_hourly in data_slice:
+    condition_code = weather_hourly["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
+
 # print(weather_data["hourly"][0]["weather"][0]["id"])
 
 
